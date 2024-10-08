d74c2ee44c8820f1cfb254bd41e32a6726297a49
frontDeveloper <gurbansahadowkemal01@gmail.com>
2024-10-08 18:24:00 +0500

    init project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client';
+import App from './components/app/App';
+
+import './style/style.scss';
+import { StrictMode } from 'react';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
